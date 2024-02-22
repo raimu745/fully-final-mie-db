51ab2e75e5dedbfb25e51e32f84ffc4c0aee6e96
Brand <umairuafmcs300@gmail.com>
2024-02-22 08:20:32 -0800

    final mie complete project with db

---
+++ vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => '1.0.0+no-version-set',
+        'version' => '1.0.0.0',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => NULL,
+        'name' => '__root__',
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => '1.0.0+no-version-set',
+            'version' => '1.0.0.0',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => NULL,
+            'dev_requirement' => false,
+        ),
+        'ejarnutowski/laravel-api-key' => array(
+            'pretty_version' => '1.0.4',
+            'version' => '1.0.4.0',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../ejarnutowski/laravel-api-key',
+            'aliases' => array(),
+            'reference' => '81ac0a4732d1a6b515f2d8ea6f4419aed7451393',
+            'dev_requirement' => false,
+        ),
+    ),
+);
